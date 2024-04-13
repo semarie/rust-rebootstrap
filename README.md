73914fdbbfea720027f1a189f50bdc608aa049ba
Sebastien Marie <semarie@kapouay.eu.org>
2024-04-13 06:42:58 +0000

    refactor rebootstrap.sh for using rust-bootstrap package

--- README.md
+++ README.md
@@ -4,9 +4,8 @@ rebootstrap.sh is a shell script for helping myself to generate bootstrap tarbal
 for [OpenBSD ports tree](http://cvsweb.openbsd.org/ports/lang/rust), for architectures
 I don't have access to (but which have already been ported).
 
-It will download snapshots elements from base (for libraries, like libc.so for example),
-and from ports (for rust, curl or others rust dependencies), and put them in a local
-directory, suitable for generating a bootstrap compatible with lang/rust port.
+It will download rust-bootstrap tarball, and compress it with tarlz for having
+a bootstrap archive compatible with lang/rust port.
 
 This way, it is possible to "regenerate" a bootstrap using fresh binaries, and ease the
 update of the bootstrap from version to version.
--- rebootstrap.sh
+++ rebootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/ksh
 #
-# Copyright (c) 2019,2022 Sebastien Marie <semarie@online.fr>
+# Copyright (c) 2019-2024 Sebastien Marie <semarie@kapouay.eu.org>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -21,39 +21,32 @@ umask 022
 ARCH=$(machine -a)
 MIRROR=$(sed 's/#.*//;/^$/d' /etc/installurl) 2>/dev/null ||
 	MIRROR='https://cdn.openbsd.org/pub/OpenBSD'
-BMIRROR=''
-
-# base mirror: see http://ftp.hostserver.de/archive/YYYY-MM-DD-ZZZZ
 
 while getopts 'a:m:b:' arg; do
 	case ${arg} in
 	a)	ARCH=${OPTARG}
 		;;
 	m)	MIRROR=${OPTARG}
-		[[ ${BMIRROR} = '' ]] && BMIRROR=${MIRROR}
 		;;
-	b)	BMIRROR=${OPTARG}
-		;;
-	*)	echo "usage: ${0##*/} [-a arch] [-m mirror] [-b basemirror]" >&2
+	*)	echo "usage: ${0##*/} [-a arch] [-m mirror]" >&2
 		exit 1
 		;;
 	esac
 done
 
-[[ ${BMIRROR} = '' ]] && BMIRROR=${MIRROR}
-
 # ensure third-parties programs to be present before running
-if ! command -v llvm-strip-16 >/dev/null; then
-	echo "error: llvm-strip-16 missing: please install llvm package" >&2
+if ! command -v tarlz >/dev/null; then
+	echo "error: tarlz missing: please install tarlz package" >&2
 	exit 1
 fi
 
-MIRRORBASE="${BMIRROR}/snapshots/${ARCH}"
 MIRRORPORTS="${MIRROR}/snapshots/packages/${ARCH}"
 
 TMPDIR=$(mktemp -d -t rebootstrap.XXXXXXXXXX) || exit 1
 PORTSINDEX="${TMPDIR}/index.txt"
 PORTSDIR="${TMPDIR}/ports"
+
+trap "rm -rf -- '${TMPDIR}'" 1 2 3 13 15 ERR EXIT
 
 # arch dependant configuration
 case "${ARCH}" in
@@ -87,118 +80,38 @@ sparc64)
 esac
 
 cat <<EOF
-==>> arch: ${ARCH}
-==>> base mirror: ${BMIRROR}
+==>> arch: ${ARCH} (${triple_arch})
 ==>> mirror: ${MIRROR}
 EOF
 
-# get base version
-set -A _KERNV -- $(sysctl -n kern.version |
-	sed 's/^OpenBSD \([1-9][0-9]*\)\.\([0-9]\)\([^ ]*\).*/\1 \2 \3/;q')
-BV=${_KERNV[0]}${_KERNV[1]}
-echo "==>> OpenBSD base${BV}"
-
-# get base signature
-ftp -Vmo "${TMPDIR}/SHA256.sig" \
-	"${MIRRORBASE}/SHA256.sig"
-
-# get base tarball
-ftp -Vmo "${TMPDIR}/base${BV}.tgz" \
-	"${MIRRORBASE}/base${BV}.tgz"
-
-# verify base tarball
-( cd "${TMPDIR}" && signify -C -p "/etc/signify/openbsd-${BV}-base.pub" -x SHA256.sig "base${BV}.tgz" )
-
 # get index of packages
 mkdir "${PORTSDIR}"
+echo "==>> Package index"
 ftp -Vmo- "${MIRRORPORTS}" \
 	| sed -ne 's|.* href="\([^"]*\.tgz\)".*|\1|p' \
 	> "${PORTSINDEX}"
 
 # get rust version
-RV=$(sed -ne 's/^rust-\([0-9].*\)\.tgz$/\1/p' "${PORTSINDEX}")
-echo "==>> Rust version ${RV}"
+RV=$(sed -ne 's/^rust-bootstrap-\([0-9].*\)\.tgz$/\1/p' "${PORTSINDEX}")
+if [ -z "${RV}" ]; then
+	echo "error: no rust-bootstrap package in index" >&2
+	exit 1
+fi
+echo "==>> Rust version: ${RV}"
 
-# process ports bootstrap dependencies
-for pkgname in rust curl nghttp2 nghttp3 libgit2 libssh2 ngtcp2 ${eports} ; do
-	pkgfile=$(grep -- "^${pkgname}-[0-9].*\.tgz" "${PORTSINDEX}")
+# get rust-bootstrap package
+pkgfile=$(grep -- "^rust-bootstrap-[0-9].*\.tgz" "${PORTSINDEX}")
 
-	# download, verify, extract
-	ftp -Vmo - "${MIRRORPORTS}/${pkgfile}" \
-		| signify -Vz -t pkg \
-		| tar zxf - -C "${PORTSDIR}"
-done
+# download, verify, extract
+ftp -Vmo - "${MIRRORPORTS}/${pkgfile}" \
+	| signify -Vz -t pkg \
+	| tar zxf - -C "${PORTSDIR}"
 
-# extract base libraries
-mkdir "${TMPDIR}/base"
-tar zxf "${TMPDIR}/base${BV}.tgz" \
-	-C "${TMPDIR}/base" \
-	'./usr/lib/lib*.so*'
+BOOTSTRAPARC="rustc-bootstrap-${ARCH}-${RV}.tar.lz"
 
-# generate bootstrap directory
-BOOTSTRAPDIR="${PWD}/rustc-bootstrap-${ARCH}-${RV}-$(date +%Y%m%d)"
-mkdir "${BOOTSTRAPDIR}"
+echo "==>> Creating bootstrap archive: ${BOOTSTRAPARC}"
+tarlz --solid -z \
+	${PORTSDIR}/lib/rustc-bootstrap-${ARCH}.tar \
+	-o "${BOOTSTRAPARC}"
 
-echo "==>> Creating bootstrap directory: ${BOOTSTRAPDIR}"
-
-# copy files for bootstrap: bin
-mkdir "${BOOTSTRAPDIR}/bin"
-for i in rustc rustdoc cargo ; do
-	cp "${PORTSDIR}/bin/$i" "${BOOTSTRAPDIR}/bin/$i"
-	llvm-strip-16 "${BOOTSTRAPDIR}/bin/$i"
-	chmod 0755 "${BOOTSTRAPDIR}/bin/$i"
-done
-
-# copy files for bootstrap: rustlib
-mkdir "${BOOTSTRAPDIR}/lib"
-cp -R "${PORTSDIR}/lib/rustlib" "${BOOTSTRAPDIR}/lib"
-find "${BOOTSTRAPDIR}/lib/rustlib" -name 'lib*.so*' -execdir llvm-strip-16 {} \;
-
-# copy required libraries
-addlibs() {
-	local f
-
-	readelf -d "$1" \
-	| sed -ne 's/.*Shared library:.*\[\([^]]*\)].*/\1/p' \
-	| while read name ; do
-		path=""
-
-		# search path (ports, base). last match wins.
-		[[ -r ${PORTSDIR}/lib/${name} ]] && path=${PORTSDIR}/lib/${name}
-		[[ -r ${TMPDIR}/base/usr/lib/${name} ]] && path=${TMPDIR}/base/usr/lib/${name}
-
-		# name not found
-		if [[ ${path} = "" ]] ; then
-			echo "error: library not found: $1: ${name}" >&2
-			printf "%s\n" "${name}" >> "${BOOTSTRAPDIR}/missing.txt"
-			continue
-		fi
-
-		# already copied, skip
-		[[ -r ${BOOTSTRAPDIR}/lib/${name} ]] && continue
-
-		# copy (or link if under rustlib/)
-		if [[ ! -r "${BOOTSTRAPDIR}/lib/rustlib/${triple_arch}/lib/${name}" ]] ; then
-			llvm-strip-16 "${path}"
-			cp "${path}" "${BOOTSTRAPDIR}/lib"
-		else
-			ln -s "rustlib/${triple_arch}/lib/${name}" "${BOOTSTRAPDIR}/lib/${name}"
-		fi
-
-		# recursively add libs
-		addlibs "${path}"
-	done
-
-	if [[ -f "${BOOTSTRAPDIR}/missing.txt" ]]; then
-		sort -u -o "${BOOTSTRAPDIR}/missing.txt" "${BOOTSTRAPDIR}/missing.txt"
-		exit 1
-	fi
-}
-
-for elf in "${BOOTSTRAPDIR}/bin/"* "${BOOTSTRAPDIR}/lib/rustlib/${triple_arch}/codegen-backends/"lib*.so ; do
-	[[ ! -r "${elf}" ]] && continue
-	addlibs "${elf}"
-done
-
-# cleaning
-rm -rf -- "${TMPDIR}"
+exit 0
